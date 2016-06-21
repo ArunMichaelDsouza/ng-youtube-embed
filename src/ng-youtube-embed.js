debfcfec5fa3888841d0219f0d230a8d86a6f020
Gustavo Salgado <gustavo.salgado@globant.com>
2016-06-21 10:06:56 -0500

    Fixing the safari ans mozilla Stric mode nested context declaration error

--- src/ng-youtube-embed.js
+++ src/ng-youtube-embed.js
@@ -35,18 +35,7 @@ angular.module('ngYoutubeEmbed', []).directive('ngYoutubeEmbed', [function() {
             var link = $scope.url;
 
             if (link != undefined) {
-                // Function to fetch id from youtube link
-                function fetchId(link) {
-                    var p = /^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
-                    var q = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
-                    var match = link.match(q);
-                    var id = link.match(p);
-                    if (id != null) {
-                        var ytId = id[1];
-                        return ytId;
-                    }
-                }
-
+                
                 // Detecting playlist and fetching video ids
                 $scope.playlistArray = [];
                 if ($scope.playlist != undefined) {
@@ -97,6 +86,18 @@ angular.module('ngYoutubeEmbed', []).directive('ngYoutubeEmbed', [function() {
                 var iframe = '<iframe width='+width+' height='+height+' src="https://www.youtube.com/embed/' + ytId + '?autoplay=' + autoplay + '&autohide=' + autohide + '&cc_load_policy=' + ccloadpolicy + '&color=' + color + '&controls=' + controls + '&disablekb=' + disablekb + '&end=' + end + '&fs=' + fs + '&hl=' + hl + '&playlist=' + playlist + '&playsinline=' + playsinline + '&rel=' + rel + '&showinfo=' + showinfo + '&start=' + start + '&theme=' + theme + '" frameborder="0" allowfullscreen></iframe>';
                 // Sanitizing and rendering iframe
                 $scope.youtubeEmbedFrame = $sce.trustAsHtml(iframe);
+            }
+
+            // Function to fetch id from youtube link
+            function fetchId(link) {
+                var p = /^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
+                var q = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+                var match = link.match(q);
+                var id = link.match(p);
+                if (id != null) {
+                    var ytId = id[1];
+                    return ytId;
+                }
             }
         }]
     }
