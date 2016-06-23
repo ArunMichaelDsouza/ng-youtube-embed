d612efba4e1ddfb4efeb69c4e6a5a9ae6cd2eb65
Arun Michael Dsouza <amdsouza92@gmail.com>
2016-06-24 00:52:44 +0530

    Url update testing - no watch added

--- demo/app.js
+++ demo/app.js
@@ -1,5 +1,12 @@
 var myApp = angular.module('myApp', ['ngYoutubeEmbed']);
 
 myApp.controller('myCtrl', ['$scope', function($scope) {
-    $scope.link = 'https://www.youtube.com/watch?v=OPmOXJtxxoo';
+    $scope.link = 'https://www.youtube.com/watch?v=9gtYRPKe-VM';
+
+    console.log($scope.link);
+
+    setTimeout(function() {
+    	$scope.link = 'https://www.youtube.com/watch?v=if32zXUXC5w';
+    	console.log($scope.link);
+    }, 5000);
 }]);
