731bd81de70c86878a6a49ba2ffb4746b8984b2e
Arun Michael Dsouza <amdsouza92@gmail.com>
2016-06-24 13:31:28 +0530

    Updated demo

--- demo/app.js
+++ demo/app.js
@@ -1,10 +1,8 @@
 var myApp = angular.module('myApp', ['ngYoutubeEmbed']);
 
 myApp.controller('myCtrl', ['$scope', function($scope) {
+	$scope.videoForm = {};
     $scope.link1 = 'https://www.youtube.com/watch?v=OPmOXJtxxoo';
     $scope.link2 = 'https://www.youtube.com/watch?v=E813VYySueM';
     $scope.link3 = 'https://www.youtube.com/watch?v=vWzeQtiwk9g';
-    $scope.updateUrl = function() {
-    	$scope.link3 = 'https://www.youtube.com/watch?v=oqdZpxkzNvc';
-    }
 }]);
