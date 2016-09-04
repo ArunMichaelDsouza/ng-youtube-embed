68874d32bc295421312dcb6be93468bbfcacdac6
Arun Michael Dsouza <amdsouza92@gmail.com>
2016-09-04 23:59:40 +0530

    Example update

--- demo/app.js
+++ demo/app.js
@@ -6,4 +6,5 @@ myApp.controller('myCtrl', ['$scope', function($scope) {
     $scope.link2 = 'https://www.youtube.com/watch?v=E813VYySueM';
     $scope.link3 = 'https://www.youtube.com/watch?v=vWzeQtiwk9g';
     $scope.link4 = 'https://gaming.youtube.com/watch?v=kNcFa3Xuk5U';
+    $scope.link5 = 'https://gaming.youtube.com/watch?v=bstDrow5goo';
 }]);
