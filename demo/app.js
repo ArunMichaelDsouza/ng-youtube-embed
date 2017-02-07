561a9f7bf19eadb401da888322e304148d4f4cb4
Arun Michael Dsouza <amdsouza92@gmail.com>
2017-02-08 00:45:25 +0530

    Demo video urls updated

--- demo/app.js
+++ demo/app.js
@@ -4,7 +4,7 @@ myApp.controller('myCtrl', ['$scope', function($scope) {
 	$scope.videoForm = {};
     $scope.link1 = 'https://www.youtube.com/watch?v=OPmOXJtxxoo';
     $scope.link2 = 'https://www.youtube.com/watch?v=E813VYySueM';
-    $scope.link3 = 'https://www.youtube.com/watch?v=vWzeQtiwk9g';
+    $scope.link3 = 'https://www.youtube.com/watch?v=SCVvhUW7cxo';
     $scope.link4 = 'https://gaming.youtube.com/watch?v=kNcFa3Xuk5U';
-    $scope.link5 = 'https://gaming.youtube.com/watch?v=bstDrow5goo';
+    $scope.link5 = 'https://gaming.youtube.com/watch?v=1pxAXJNJ-z8';
 }]);
