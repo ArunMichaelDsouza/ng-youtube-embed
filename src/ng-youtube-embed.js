52bef94e44022af6fdf6b699366a1ab155ce6537
--global <amdsouza92@gmail.com>
2017-04-29 03:00:12 +0530

    Youtube mobile url matching bug fix

--- src/ng-youtube-embed.js
+++ src/ng-youtube-embed.js
@@ -14,7 +14,7 @@
     // Module globals
     var VIDEO_IDS = [],
         YOUTUBE_IFRAME_EMBED_API = 'https://www.youtube.com/iframe_api',
-        YOUTUBE_VIDEO_ID_REGEX = /^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/,
+        YOUTUBE_VIDEO_ID_REGEX = /^(?:https?:\/\/)?(?:www\.|m\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/,
         YOUTUBE_VIDEO_ID_REGEX_GAMING = /^(?:https?:\/\/)?(?:www\.)?(?:gaming.youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
 
     // Function to load youtube iframe embed api
