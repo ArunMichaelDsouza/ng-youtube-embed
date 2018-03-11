05fff6b62243c6227d7325a6b9ecd42308f7eff5
--global <amdsouza92@gmail.com>
2018-03-11 20:46:27 +0530

    Build version updated

--- src/ng-youtube-embed.js
+++ src/ng-youtube-embed.js
@@ -1,5 +1,5 @@
 /*
-    ng-youtube-embed v1.7.11
+    ng-youtube-embed v1.7.12
     Copyright (c) 2015 Arun Michael Dsouza (amdsouza92@gmail.com)
     Licence: MIT
     Demo on CodePen - http://codepen.io/amdsouza92/pen/yNxyJV
